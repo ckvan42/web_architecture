bc49d19728b083d6bb8731db54f09a1ece1ff07c
ckvan42 <giwounbae@gmail.com>
2022-01-13 11:43:02 -0800

    add update time feature

--- lab1/js/writer.js
+++ lab1/js/writer.js
@@ -1,9 +1,10 @@
 const msg_notSupported = "Sorry web Storage is not supported";
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 /**
  * Global variable for keeping the id. 
  */
-counter = 0; 
+counter = 0;
 let arrayTextBoxes = [];
 
 
@@ -16,7 +17,7 @@ let arrayTextBoxes = [];
  * 
  * It reads from the current localStorage and populate them as Notes.
  */
-window.onload = function() {
+window.onload = function () {
   //need to populate the array from the localStorage
   for (let i = 0; i < localStorage.length; i++) {
     let initialNote = new Note(localStorage.key(i));
@@ -24,8 +25,7 @@ window.onload = function() {
     arrayTextBoxes.push(initialNote);
     counter++;
   }
-  if (localStorage.length == 0)
-  {
+  if (localStorage.length == 0) {
     arrayTextBoxes.push(new Note(counter++));
   }
 }; //first Note when first loaded. 
@@ -44,19 +44,17 @@ function addEachNote() {
 }
 
 function goBackHome() {
-  location.href="./index.html";
+  location.href = "./index.html";
 }
 
 
 /**
  * TextBox object
  */
-class Note
-{
-  constructor(key)
-  {
+class Note {
+  constructor(key) {
     this._key = key;
-    this._btn = createRemoveBtn(this._key);//remove button
+    this._btn = createRemoveBtn(this._key); //remove button
     this._noteBox = createNoteBox(this._key); //noteBox
     this._btn.onclick = this.deleteFromStorage;
     this._noteContainer = createNoteContainer(this._key, this._btn, this._noteBox);
@@ -67,60 +65,53 @@ class Note
     this._noteBox.innerHTML = content;
   }
 
-  set setKey (key)
-  {
-    this._key = key;  
+  set setKey(key) {
+    this._key = key;
   }
 
-  get getContent()
-  {
+  get getContent() {
     return this._content;
   }
 
   //A method to save to localStorage.
-  saveToStorage = function() {
-    if(typeof(Storage) != "undefined")
-    {
+  saveToStorage = function () {
+    if (typeof (Storage) != "undefined") {
       localStorage.setItem(this._key, this._noteBox.value);
-    } else 
-    {
+    } else {
       alert(msg_notSupported);
     }
   }
 
-  deleteFromStorage = function() {
+  deleteFromStorage = function () {
     localStorage.removeItem(this.id);
     //clearing textbox
     let tb = document.getElementById("container_" + this.id);
     tb.value = "";
     this.parentNode.remove();
     updateStorageArray(this.id);
-  } 
+  }
 
-  setRemoveButtonHandler = function()
-  {
+  setRemoveButtonHandler = function () {
     let button = this._btn;
-    
+
     button.onclick = deleteFromStorage;
   }
-  
+
   setRemoveButtonHandler;
 }
 
-function updateStorageArray(id)
-{
+function updateStorageArray(id) {
   //I have to delete from the array too.
   let index = -1;
   for (let i = 0; i < arrayTextBoxes.length; i++) {
-    if(arrayTextBoxes[i]._key === id) {
+    if (arrayTextBoxes[i]._key === id) {
       index = i;
     }
   }
   arrayTextBoxes.splice(index, 1);
 }
 
-function createNoteContainer(idCounter, noteBox, btn)
-{
+function createNoteContainer(idCounter, noteBox, btn) {
   let noteContainer;
   noteContainer = document.createElement("div");
   noteContainer.className = "each_note";
@@ -134,21 +125,20 @@ function createNoteContainer(idCounter, noteBox, btn)
 /**
  * create textbox.
  */
-function createNoteBox(idcounter)
-{
-   //create a div with textbox and button.
+function createNoteBox(idcounter) {
+  //create a div with textbox and button.
   let textbox = document.createElement("TEXTAREA");
   textbox.classList.add("form-control");
   textbox.rows = "3";
-  textbox.placeholder="Please write notes.";
+  textbox.placeholder = "Please write notes.";
   textbox.id = "each_note_" + idcounter;
   return textbox;
 }
 
-function createRemoveBtn(id)
-{
+function createRemoveBtn(id) {
   //each remove button should have unique id that corresponds to the textbox.
   let rvBtn = document.createElement("BUTTON");
+  rvBtn.className = "btn btn-outline-primary";
   rvBtn.classList.add("remove_btn");
   rvBtn.id = id;
   rvBtn.innerHTML = "REMOVE";
@@ -162,13 +152,31 @@ function createRemoveBtn(id)
 /**
  * saves to localStorage every 2 seconds
  */
-setInterval(function() {
+setInterval(function () {
   arrayTextBoxes.forEach(note => {
-    console.log(note._noteBox.value);
-    if (note._noteBox.value != "")
-    {
+    if (note._noteBox.value != "") {
       note.saveToStorage();
+      updateTime();
     }
   })
 }, 2000);
 
+/**
+ * Updates time
+ * 
+ * Original src: https://stackoverflow.com/questions/5091888/how-to-update-time-regularly
+ */
+function updateTime() {
+  const date = new Date();
+  const twoDigitMinutes = date.toLocaleString("en-us", {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: "2-digit"
+  });
+
+  document.getElementById('time').innerHTML = "Last updated: " + twoDigitMinutes;
+}
+updateTime();
